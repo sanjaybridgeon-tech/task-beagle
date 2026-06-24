ee4d0b4aec0d1136c3f2312353ded86316d66f96
Sanjay Atholi <sanjaysr2902@gmail.com>
2026-06-24 12:39:19 +0530

    ffffhhiuujn

--- src/main/java/task1/optionalQn.java
+++ src/main/java/task1/optionalQn.java
@@ -10,7 +10,7 @@ public class optionalQn {
 
         Optional<String> name = getName();
 
-        System.out.println(name.orElse("Guest"));
+        System.out.println(name.orElse("Guessssssst"));
     }
 
     public Optional<String> getName() {
